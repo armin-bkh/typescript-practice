5baff8bb92eec62dbd700b25001694006227962c
armin bakhshi <arminbkh0921@yahoo.com>
2021-12-14 14:30:03 +0330

    array and objects in typescript

--- sandbox.js
+++ sandbox.js
@@ -8,3 +8,43 @@ var calc = function (num) {
     return num * num;
 };
 console.log(calc(5));
+var students = ["armin", "matin", "rading"];
+// students[0] = 0;
+students[0] = "reza";
+students.push("mamd");
+var numbers = [20, 17, 15, 19];
+numbers.push(10);
+var mixArray = [0, "mnamda", { name: "armin" }, false];
+mixArray.push(5);
+mixArray.push("asdsa");
+mixArray.push(true);
+var person = {
+    name: "ali",
+    age: 14,
+    family: "zandi"
+};
+person.name = "sadegh";
+// person.name = 15;
+person.age = 16;
+// person.age = "adf" 
+// person.skill = ["react", "vue"];
+person = {
+    name: "reza",
+    age: 20,
+    family: "rahimi"
+};
+// person = {
+//   name: "reza",
+//   age: 20,
+// }
+// person = {
+//   name: "reza",
+//   age: "asd",
+//   family: 12,
+// }
+// person = {
+//   name: "reza",
+//   age: 20,
+//   family: "rahimi",
+//   skills: ['react', 'vue'],
+// }
--- sandbox.ts
+++ sandbox.ts
@@ -11,5 +11,54 @@ inputs.forEach(input => {
 const calc = (num : number) =>{
   return num * num;
 }
-
 console.log(calc(5));
+
+let students = ["armin", "matin", "rading"];
+// students[0] = 0;
+students[0] = "reza";
+students.push("mamd");
+
+let numbers = [20 ,17, 15, 19];
+numbers.push(10);
+
+let mixArray = [0, "mnamda", { name: "armin" }, false];
+
+mixArray.push(5);
+mixArray.push("asdsa");
+mixArray.push(true);
+
+let person = {
+  name: "ali",
+  age: 14,
+  family: "zandi",
+}
+
+person.name = "sadegh";
+// person.name = 15;
+person.age = 16;
+// person.age = "adf" 
+// person.skill = ["react", "vue"];
+
+person = {
+  name: "reza",
+  age: 20,
+  family: "rahimi"
+}
+
+// person = {
+//   name: "reza",
+//   age: 20,
+// }
+
+// person = {
+//   name: "reza",
+//   age: "asd",
+//   family: 12,
+// }
+
+// person = {
+//   name: "reza",
+//   age: 20,
+//   family: "rahimi",
+//   skills: ['react', 'vue'],
+// }

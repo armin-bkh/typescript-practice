8a8567542027b46cea52c506ab0b5e3d39fc2a1b
armin bakhshi <arminbkh0921@yahoo.com>
2021-12-14 20:37:49 +0330

    explicit types and union types

--- sandbox.js
+++ sandbox.js
@@ -48,3 +48,44 @@ person = {
 //   family: "rahimi",
 //   skills: ['react', 'vue'],
 // }
+//explicit types
+var Name;
+Name = "132";
+var age;
+age = 15;
+var isAdmin;
+isAdmin = false;
+var teachers = [];
+teachers.push("Eslami");
+var counts = [];
+counts.push(1);
+//explicit types
+//union types
+var mixed = [];
+mixed.push({ name: "armin" });
+mixed.push(15);
+mixed.push("rrr");
+mixed.push(false);
+mixed.push(["armin", "reza", 5]);
+console.log(mixed);
+var counter;
+counter = 5;
+counter = "5";
+console.log(counter);
+//union types
+// explicit type in object
+//explicit type in object with allow array
+var you;
+you = [];
+//explicit type in object with allow array
+//explicit type in object with specifc
+var me;
+me = {
+    name: "armin",
+    family: "bakhshi",
+    age: 18,
+    isAdmin: true
+};
+console.log(me);
+//explicit type in object with specifc
+// explicit type in object
--- sandbox.ts
+++ sandbox.ts
@@ -62,3 +62,74 @@ person = {
 //   family: "rahimi",
 //   skills: ['react', 'vue'],
 // }
+
+
+//explicit types
+
+let Name : string;
+Name = "132";
+
+let age : number;
+age = 15;
+
+let isAdmin : boolean;
+isAdmin = false;
+
+
+let teachers : string[] = [];
+teachers.push("Eslami");
+let counts : number[] = [];
+counts.push(1);
+//explicit types
+
+
+//union types
+
+let mixed : (string|number|{ name: string }|boolean|object)[] = [];
+mixed.push({ name: "armin" });
+mixed.push(15);
+mixed.push("rrr");
+mixed.push(false);
+mixed.push(["armin", "reza", 5]);
+console.log(mixed);
+
+
+let counter : number|string;
+counter = 5;
+counter = "5";
+console.log(counter);
+
+//union types
+
+// explicit type in object
+
+//explicit type in object with allow array
+
+let you : object;
+you = [];
+
+//explicit type in object with allow array
+
+
+//explicit type in object with specifc
+
+let me: {
+  name: string,
+  family: string,
+  age: number,
+  isAdmin: boolean,
+}
+
+
+me = {
+  name: "armin",
+  family: "bakhshi",
+  age: 18,
+  isAdmin: true,
+}
+console.log(me);
+
+//explicit type in object with specifc
+
+
+// explicit type in object
